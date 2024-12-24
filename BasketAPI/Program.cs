02bd55c859ab8165d5fa30e9f5dc6d323cf195d6
Vinicius Silva <vsaguiar@outlook.com>
2024-12-24 20:25:29 -0300

    conectando os containers catalog e basket

--- BasketAPI/Program.cs
+++ BasketAPI/Program.cs
@@ -23,7 +23,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "BasketAPI v1");
+    });
 }
 
 app.UseAuthorization();
